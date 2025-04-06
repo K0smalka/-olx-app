e6e59974a3948d1d43968dc241fbe24bc6a379e9
K0smalka <284325@student.pwr.edu.pl>
2025-04-06 18:14:47 +0200

    feat: search queries controller

--- start/routes.ts
+++ start/routes.ts
@@ -8,8 +8,12 @@
 */
 import router from "@adonisjs/core/services/router";
 
+import SearchQueriesController from "#controllers/search_queries_controller";
+
 router.get("/", async () => {
   return {
     hello: "world",
   };
 });
+
+router.post("/test", [SearchQueriesController, "store"]);
