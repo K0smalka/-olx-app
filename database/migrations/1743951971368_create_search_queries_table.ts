50611c8ee2a91933789a87c7538b11ff53aa4ad7
K0smalka <284325@student.pwr.edu.pl>
2025-04-06 17:41:38 +0200

    feat: request search query migration

--- database/migrations/1743951971368_create_search_queries_table.ts
+++ database/migrations/1743951971368_create_search_queries_table.ts
@@ -1,24 +1,26 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'search_queries'
+  protected tableName = "search_queries";
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments("id");
 
-      table.float('price_minimum')
-      table.float('price_maximum')
+      table.float("price_minimum");
+      table.float("price_maximum");
 
-      table.integer('location_id')
+      table.integer("location_id");
 
-      table.timestamp('refreshed_at')
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
-    })
+      table.timestamp("refreshed_at");
+
+      table.timestamp("created_at");
+
+      table.timestamp("updated_at");
+    });
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
